00b42ef97b39c68fa48e54e7dec58efc2345a4dc
Adrian Tomasek <adrian.tomasek@gmail.com>
2021-01-23 13:06:39 +0100

    - fix menu in impressum - chart alles von anfang sichtbar

--- chart-generator.js
+++ chart-generator.js
@@ -19,8 +19,8 @@ var chart = new Chart(ctx, {
             steppedLine: 'true',
             borderColor: 'fuchsia',
             borderWidth: 1,
-            backgroundColor: 'transparent',
-            hidden: true
+            backgroundColor: 'transparent'
+            /* hidden: true */
         },
         {
             label: 'Sonntag, 02.02.2020',
@@ -29,8 +29,8 @@ var chart = new Chart(ctx, {
             steppedLine: 'true',
             borderColor: 'orange',
             borderWidth: 1,
-            backgroundColor: 'transparent',
-            hidden: true
+            backgroundColor: 'transparent'
+            /* hidden: true */
         },
         {
             label: 'Freitag, 07.02.2020',
@@ -39,8 +39,8 @@ var chart = new Chart(ctx, {
             steppedLine: 'true',
             borderColor: 'green',
             borderWidth: 1,
-            backgroundColor: 'transparent',
-            hidden: true
+            backgroundColor: 'transparent'
+            /* hidden: true */
         },
         {
             label: 'Samstag, 08.02.2020',
@@ -49,8 +49,8 @@ var chart = new Chart(ctx, {
             steppedLine: 'true',
             borderColor: 'purple',
             borderWidth: 1,
-            backgroundColor: 'transparent',
-            hidden: true
+            backgroundColor: 'transparent'
+            /* hidden: true */
         },
         {
             label: 'Sonntag, 09.02.2020',
@@ -59,8 +59,8 @@ var chart = new Chart(ctx, {
             steppedLine: 'true',
             borderColor: 'grey',
             borderWidth: 1,
-            backgroundColor: 'transparent',
-            hidden: true
+            backgroundColor: 'transparent'
+            /* hidden: true */
         }],
         labels: ["10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00",
             "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30"],
